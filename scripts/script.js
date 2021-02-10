8c9c35deac014333f39fe25e4adbd0ec7f4db406
DanielChenOfficial2 <53456446+DanielChenOfficial2@users.noreply.github.com>
2021-02-09 23:34:26 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,7 @@ window.addEventListener('DOMContentLoaded', () => {
         localStorage.setItem(`${product.id}`, 'test');
           
       }  
-    }
+    })
   myStorage = window.localStorage;
   localStorage.setItem('mygame', 'PriCon');
 });

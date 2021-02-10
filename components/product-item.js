a0668cbcf711effdc91a41c0baf85350c7fc2fdc
DanielChenOfficial2 <53456446+DanielChenOfficial2@users.noreply.github.com>
2021-02-10 00:41:33 -0800

    Update product-item.js

--- components/product-item.js
+++ components/product-item.js
@@ -2,6 +2,12 @@
 
 class ProductItem extends HTMLElement {
   // TODO
+  constructor() {
+    super();
+    this.attachShadow({mode:'open'});
+    
+    const wrapper = document.createElement
+  }
 }
 
 customElements.define('product-item', ProductItem);

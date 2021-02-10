9aaeafd89b5202e1d056fc6867c5455fc006d502
DanielChenOfficial2 <53456446+DanielChenOfficial2@users.noreply.github.com>
2021-02-09 21:36:09 -0800

    test if json get added to localstorage

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,12 @@
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
   fetch('https://fakestoreapi.com/products')
-    .then(response => response.json());
+    .then(response => response.json())
+    .then(data => {
+      for (const product of data.products) {
+        localStorage.setItem(${product.id}, 'test')
+          
+      }  
   myStorage = window.localStorage;
   localStorage.setItem('mygame', 'PriCon');
 });
